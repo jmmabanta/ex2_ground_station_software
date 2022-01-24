4843bd17a579f35403f35d0e7b2046dbabe2fc25
John M <johnmabanta51@gmail.com>
2022-01-23 21:40:09 -0700

    Now mounts src/groundStation/dev.db symlink

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export GS_HOME=$(pwd)
-docker run --rm -it -v $GS_HOME:/home/gs ground_station:latest
+docker run --rm -it -v $GS_HOME:/home/gs -v $(readlink -f src/groundStation/dev.db):/home/gs/src/groundStation/dev.db ground_station:latest
